b53daa7d58b759fa598bf3f2ef5a6ec00eed9105
Joel Brown <joelsbrown1@gmail.com>
2015-06-04 17:06:45 -0700

    untabify

--- test/run.cpp
+++ test/run.cpp
@@ -12,9 +12,9 @@ TEST_CASE("mssql datasource") {
     //This will be executed before each section
     
     SECTION("register plugin") 
-	{
-		std::string mssql_plugin = "./mssql.input";
-		REQUIRE(mapnik::datasource_cache::instance().register_datasource(mssql_plugin));
+    {
+        std::string mssql_plugin = "./mssql.input";
+        REQUIRE(mapnik::datasource_cache::instance().register_datasource(mssql_plugin));
     }
 }
 
@@ -22,17 +22,17 @@ char * _connection_string = NULL;
 
 int main (int argc, char* const argv[])
 {
-	//get connection string
-	_connection_string = std::getenv("MSSQL_CONNECTION_STRING");
-	if (_connection_string == NULL)
-	{
-		_connection_string = "Driver={SQL Server};Server=.;Database=master;Trusted_Connection=Yes;";
-	}
+    //get connection string
+    _connection_string = std::getenv("MSSQL_CONNECTION_STRING");
+    if (_connection_string == NULL)
+    {
+        _connection_string = "Driver={SQL Server};Server=.;Database=master;Trusted_Connection=Yes;";
+    }
 
-	//run tests
+    //run tests
     int result = Catch::Session().run( argc, argv );
 
-	//clean up
+    //clean up
     // http://icu-project.org/apiref/icu4c/uclean_8h.html#a93f27d0ddc7c196a1da864763f2d8920
     u_cleanup();
     return result;
