89f4ece5e769c860e275609b21ce79813fa68e92
Joel Brown <joelsbrown1@gmail.com>
2015-06-12 15:54:48 -0600

    update bootstrap [skip ci]

--- bootstrap.ps1
+++ bootstrap.ps1
@@ -6,6 +6,3 @@ if ($ENV:PLATFORM -eq $null)
 (New-Object Net.WebClient).DownloadFile("https://mapnik.s3.amazonaws.com/dist/dev/vs2015rc1/mapnik-win-sdk-14.0-$ENV:PLATFORM-v3.0.0-rc1-1158-g73dbec3.7z", "$pwd\mapnik-win-sdk.7z")
 & 7z x .\mapnik-win-sdk.7z "-o$pwd"
 
-$MAPNIK_SDK = "$pwd\mapnik-sdk"
-$ENV:PATH = "$MAPNIK_SDK\lib" + ";" + $ENV:Path
-$ENV:ICU_DATA = "$MAPNIK_SDK\share\icu"
